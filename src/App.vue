8dfd7e8439861269eb3605f1a0cba2b5218d536a
Ewerton Batista <ewerton13batista@gmail.com>
2021-07-15 20:16:52 -0300

    Criado funcionalidades
    Funcionalidade de inserir criada

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Preencha as informações abaixo</h1>
+
+      <CreateUser/>
+      <hr>
+      <ReadUser/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {eventBus} from './main'
+import CreateUser from './components/CreateUser.vue'
+import ReadUser from './components/ReadUser.vue'
+
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    CreateUser,
+    ReadUser
+  },
+  data(){
+    return {
+      dados: {
+        type: Object
+      }
+    }
+  },
+  created(){
+    eventBus.$on('salvar', (dados) => {
+      this.dados = dados
+    })
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  #app {
+    width: 960px;
+    margin: 0 auto;
+    border: 1px solid blue;
+  }
 </style>
---
+++ src/components/CreateUser.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <form action="" @submit.prevent="salvar">
+            <label for="nome">Nome:</label>
+            <input 
+            required 
+            type="text" 
+            name="nome"
+            v-model="newUser.nome">
+
+            <label for="idade">Idade:</label>
+            <input 
+            required 
+            type="text" 
+            name="idade"
+            v-model.number="newUser.idade">
+
+            <label for="email"></label>
+            <input
+            required 
+            type="email" 
+            name="email"
+            v-model="newUser.email">
+
+            <button type="submit">salvar</button>
+        </form>
+        
+    </div>
+</template>
+
+<script>
+
+    // Imports
+import {eventBus} from './../main'
+
+export default {
+
+    // Data
+    data(){
+        return {
+            newUser: {
+                nome: '',
+                idade: 18,
+                email: ''
+            },
+            finalUser: {},
+            finalUsers: []
+        }
+    },
+    // Methods
+    methods: {
+        salvar(){
+            this.finalUser = Object.assign({}, this.newUser)
+            this.finalUsers.push(this.finalUser)
+            eventBus.$emit('cadastrar', this.finalUsers)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
